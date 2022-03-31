b1526ba8281731a53ff9b9e07f81ea4809589359
James Correa <james.correa9090@gmail.com>
2022-03-31 10:49:02 -1000

    routing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
-
-import { useState } from 'react';
-import './App.css';
-import Login from './components/Login';
-import Signup from './components/Signup';
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
 
 function App() {
   const [user, setUser] = useState({});
 
-
   return (
     <div className="App">
-      <Signup user={user} setUser={setUser}/>
-      <Login user={user} setUser={setUser}/>
+      <Routes>
+        <Route
+          path="/signup"
+          element={<Signup user={user} setUser={setUser} />}
+        />
+        <Route
+          path="/login"
+          element={<Login user={user} setUser={setUser} />}
+        />
+      </Routes>
     </div>
   );
 }
