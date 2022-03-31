8baca0714b93504079e5934cc5d54003744efd33
James Correa <james.correa9090@gmail.com>
2022-03-31 10:35:53 -1000

    error handling

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,48 @@
-import React from 'react'
+import React from "react";
 
-function Login() {
+function Login({ user, setUser }) {
+  function handleLogin(e) {
+    e.preventDefault();
+    console.log(e);
+    setUser({});
+    fetch(`http://localhost:3000/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: e.target[0].value,
+        password: e.target[1].value,
+      }),
+    })
+      // .then((r) => r.json())
+      .then((r) => {
+        if (r.ok) {
+          console.log(r);
+          r.json().then((data) => {
+            localStorage.setItem("token", data.jwt);
+            console.log(data.jwt);
+            setUser(data);
+          });
+        } else {
+          r.json().then((err) => {
+            console.log(err);
+            setUser({});
+          });
+        }
+      });
+  }
+
   return (
-    <div>Login</div>
-  )
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={handleLogin}>
+        <input type="text" placeholder="username" />
+        <input type="password" placeholder="password" />
+        <input type="submit" value="submit" />
+      </form>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,38 +1,45 @@
-import React from 'react';
+import React from "react";
 
-function Signup( {user, setUser} ) {
-
- function handleSignup(e) {
-     e.preventDefault()
+function Signup({ user, setUser }) {
+  function handleSignup(e) {
+    e.preventDefault();
     // console.log(e.target[1].value)
     fetch(`http://localhost:3000/users`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            Accept: "application/json",
-        },
-        body: JSON.stringify({
-            username: e.target[0].value,
-            password: e.target[1].value
-        }),
-    })
-    .then((resp) => resp.json())
-    .then((data) => {
-        console.log(data)
-        localStorage.setItem("token", data.jwt)
-        setUser(data.user);
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({
+        username: e.target[0].value,
+        password: e.target[1].value,
+      }),
+    }).then((r) => {
+      if (r.ok) {
+        r.json().then((data) => {
+          localStorage.setItem("token", data.jwt);
+          console.log(data.jwt);
+          setUser(data);
+        });
+      } else {
+        r.json().then((err) => {
+          console.log(err);
+          setUser({});
+        });
+      }
     });
- }
+  }
 
   return (
     <div>
-        <form onSubmit={handleSignup}>
-            <input type="text" placeholder="username" />
-            <input type="password" placeholder="password" />
-            <input type="submit" value="submit" />
-        </form>
+      <h1>Signup</h1>
+      <form onSubmit={handleSignup}>
+        <input type="text" placeholder="username" />
+        <input type="password" placeholder="password" />
+        <input type="submit" value="submit" />
+      </form>
     </div>
-  )
+  );
 }
 
-export default Signup
+export default Signup;
